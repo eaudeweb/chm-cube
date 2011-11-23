c5885ea58a761dd9ab6c9a29e84a05efd358cbcb
Alex Morega <alex@grep.ro>
2011-11-23 23:12:39 +0200

    parse custom apache log file

---
+++ logpump.py
@@ -0,0 +1,67 @@
+from __future__ import division
+import sys
+from collections import namedtuple
+import re
+from dateutil.parser import parse as parse_date
+
+
+LogEntry = namedtuple('LogEntry', 'datetime duration method url vhost '
+                                  'status size useragent client')
+
+def parse_log(input_lines):
+    pattern = re.compile(
+        r'^(?P<client>\S+) \S+ \S+ '
+        r'\[(?P<datetime>[^\]]+)\] '
+        r'"(?P<method>\w+) (?P<url>\S*)( [^"]+)?" '
+        r'(?P<status>\d+) '
+        r'(?P<size>\S+) ' # number or "-"
+        r'"(?P<referrer>[^"]*)" '
+        r'"(?P<useragent>([^"]|\")+)" ' # referrer may contain '\"'
+        r'(?P<duration>\d+) '
+        r'(?P<vhost>\S+)$')
+
+    for n, line in enumerate(input_lines):
+        match = pattern.match(line.strip())
+        assert match is not None, "line %d does not match: %r" % (n, line)
+        str_size = match.group('size')
+        size = (0 if str_size == '-' else int(str_size))
+        rdt = match.group('datetime')
+        yield LogEntry(datetime=parse_date(rdt[:11] + ' ' + rdt[12:]),
+                       duration=int(match.group('duration')) / 10**6,
+                       method=match.group('method'),
+                       url=match.group('url'),
+                       vhost=match.group('vhost'),
+                       status=match.group('status'),
+                       size=size,
+                       useragent=match.group('useragent'),
+                       client=match.group('client'))
+
+
+def pump_to_cube(entry_list, url):
+    def send_batch():
+        print batch
+        batch[:] = []
+
+    batch = []
+    for entry in entry_list:
+        event = {
+            'type': 'chm-eu-request',
+            'time': entry.datetime.isoformat(),
+        }
+        batch.append(event)
+
+        if len(batch) >= 100:
+            send_batch()
+            return
+
+    if batch:
+        send_batch()
+
+
+def main():
+    url = "http://localhost:1080/1.0/event/put"
+    pump_to_cube(parse_log(sys.stdin), url)
+
+
+if __name__ == '__main__':
+    main()
