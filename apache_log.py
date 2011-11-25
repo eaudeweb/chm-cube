39a4ab385896ffaa1ee4a00a0ff1c9886e3b4c35
Alex Morega <alex@grep.ro>
2011-11-25 10:55:56 +0200

    factor out `parse_log`

---
+++ apache_log.py
@@ -0,0 +1,36 @@
+from __future__ import division
+from collections import namedtuple
+import re
+from dateutil.parser import parse as parse_date
+
+
+LogEntry = namedtuple('LogEntry', 'datetime duration method url vhost '
+                                  'status size useragent client')
+
+def parse_log(input_lines):
+    pattern = re.compile(
+        r'^(?P<client>\S+) \S+ \S+ '
+        r'\[(?P<datetime>[^\]]+)\] '
+        r'"(?P<method>\w+) (?P<url>\S*)( [^"]+)?" '
+        r'(?P<status>\d+) '
+        r'(?P<size>\S+) ' # number or "-"
+        r'"(?P<referrer>([^"]|\\")*)" ' # referrer may contain '\"'
+        r'"(?P<useragent>([^"]|\\")+)" ' # user agent may contain '\"'
+        r'(?P<duration>\d+) '
+        r'(?P<vhost>\S+)$')
+
+    for n, line in enumerate(input_lines):
+        match = pattern.match(line.strip())
+        assert match is not None, "line %d does not match: %r" % (n, line)
+        str_size = match.group('size')
+        size = (0 if str_size == '-' else int(str_size))
+        rdt = match.group('datetime')
+        yield LogEntry(datetime=parse_date(rdt[:11] + ' ' + rdt[12:]),
+                       duration=int(match.group('duration')) / 10**6,
+                       method=match.group('method'),
+                       url=match.group('url'),
+                       vhost=match.group('vhost'),
+                       status=int(match.group('status')),
+                       size=size,
+                       useragent=match.group('useragent'),
+                       client=match.group('client'))
--- logpump.py
+++ logpump.py
@@ -1,47 +1,12 @@
-from __future__ import division
 import sys
 import urllib
 import logging
-from collections import namedtuple
-import re
 import simplejson as json
-from dateutil.parser import parse as parse_date
+from apache_log import parse_log
 
 
 log = logging.getLogger('logpump')
 log.setLevel(logging.DEBUG)
-
-
-LogEntry = namedtuple('LogEntry', 'datetime duration method url vhost '
-                                  'status size useragent client')
-
-def parse_log(input_lines):
-    pattern = re.compile(
-        r'^(?P<client>\S+) \S+ \S+ '
-        r'\[(?P<datetime>[^\]]+)\] '
-        r'"(?P<method>\w+) (?P<url>\S*)( [^"]+)?" '
-        r'(?P<status>\d+) '
-        r'(?P<size>\S+) ' # number or "-"
-        r'"(?P<referrer>([^"]|\\")*)" ' # referrer may contain '\"'
-        r'"(?P<useragent>([^"]|\\")+)" ' # user agent may contain '\"'
-        r'(?P<duration>\d+) '
-        r'(?P<vhost>\S+)$')
-
-    for n, line in enumerate(input_lines):
-        match = pattern.match(line.strip())
-        assert match is not None, "line %d does not match: %r" % (n, line)
-        str_size = match.group('size')
-        size = (0 if str_size == '-' else int(str_size))
-        rdt = match.group('datetime')
-        yield LogEntry(datetime=parse_date(rdt[:11] + ' ' + rdt[12:]),
-                       duration=int(match.group('duration')) / 10**6,
-                       method=match.group('method'),
-                       url=match.group('url'),
-                       vhost=match.group('vhost'),
-                       status=int(match.group('status')),
-                       size=size,
-                       useragent=match.group('useragent'),
-                       client=match.group('client'))
 
 
 def pump_to_cube(entry_list, url):
