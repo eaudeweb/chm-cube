5cc75909137a38ef686322e5753f384a7cf4c62c
Alex Morega <alex@grep.ro>
2011-11-27 21:13:51 +0200

    generate cube events

--- logtail.py
+++ logtail.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 import time
 import logging
 import simplejson as json
+from apache_log import parse_log_line, cube_event_for_entry
 
 """
 read apache log file incrementally
@@ -23,7 +24,7 @@ class LogReader(object):
         self.log_dir = log_dir
         self.log_basename = log_basename
 
-    def stream_log_entries(self):
+    def log_lines(self):
         log_file_path = os.path.join(self.log_dir, self.log_basename)
         log_file = open(log_file_path, 'rb')
 
@@ -32,8 +33,6 @@ class LogReader(object):
 
         while True:
             buf += log_file.read(BUFFER_SIZE)
-            if buf:
-                log.debug("buffer is now %r" % buf)
             if not buf:
                 # end-of-file: see if logging happens in another file
                 # TODO check for another file
@@ -47,78 +46,43 @@ class LogReader(object):
                 if newline < 1:
                     # roll the buffer, only keep unhandled line fragment
                     buf = buf[prev_newline:]
-                    log.debug("rolled buffer to %d, fragment is %r",
-                              prev_newline, buf)
                     prev_newline = 0
                     break
 
-                log.debug("current line from %d to %d", prev_newline, newline)
                 line = buf[prev_newline:newline]
-                self.handle(line)
+                yield line
                 prev_newline = newline
 
-    def handle(self, line):
-        log.debug("handling line %r", line)
+    def determine_log_file_name(self):
+        current_filename = os.path.join(self.log_dir, self.log_basename)
+        old_filename = current_filename + '.1'
+
+        old_mtime = datetime.fromtimestamp(os.stat(old_filename).st_mtime)
+        old_year, old_week, old_weekday = old_mtime.isocalendar()
+        assert old_weekday == 7
+        year, week, n_weekday = (old_mtime + timedelta(days=7)).isocalendar()
+        assert n_weekday == 7
+
+        return 'chm-eu/%d-%d' % (year, week)
+
+    def stream_log_entries(self):
+        log_file_name = self.determine_log_file_name()
+
+        for n, line in enumerate(self.log_lines()):
+            entry = parse_log_line(line)
+            event = cube_event_for_entry(entry)
+            event['id'] = "%s/%d" % (log_file_name, n)
+            print json.dumps(event)
 
 
 def main():
-    from apache_log import cube_events
-
     log_dir, log_basename = sys.argv[1:3]
-
     log_reader = LogReader(log_dir, log_basename)
     log_reader.stream_log_entries()
-    return
-
-
-
-
-
-    current_filename = sys.argv[1]
-    old_filename = current_filename + '.1'
-
-    old_mtime = datetime.fromtimestamp(os.stat(old_filename).st_mtime)
-    old_year, old_week, old_weekday = old_mtime.isocalendar()
-    assert old_weekday == 7
-    year, week, n_weekday = (old_mtime + timedelta(days=7)).isocalendar()
-    assert n_weekday == 7
-
-    log_file_name = 'chm-eu/%d-%d' % (year, week)
-
-    bytes_offset = int(sys.argv[2]) if len(sys.argv) > 2 else 0
-
-    log_file = open(current_filename)
-    log_file.seek(bytes_offset)
-
-    log_file_state = {'bytes': bytes_offset}
-    def log_file_lines():
-        for line in log_file:
-            log_file_state['bytes'] += len(line)
-            yield line
-
-    batch_size = 10000
-    count = 0
-    for n, event in enumerate(cube_events(log_file_lines())):
-        event['id'] = "%s/%d" % (log_file_name, n)
-        print json.dumps(event)
-        count += 1
-        if count >= batch_size:
-            has_more = True
-            break
-    else:
-        has_more = False
-
-    log_file.close()
-
-    print json.dumps({
-        'logtail_end': True,
-        'logtail_has_more': has_more,
-        'logtail_bytes_offset': log_file_state['bytes'],
-    })
 
 
 if __name__ == '__main__':
-    logging.basicConfig(loglevel=logging.DEBUG)
+    logging.basicConfig(loglevel=logging.INFO)
     BUFFER_SIZE = 46
     SLEEP_TIME = 1
     main()
