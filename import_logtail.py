f3b84d50f6ed683881c753f4baf576b16fef40da
Alex Morega <alex@grep.ro>
2011-11-27 21:51:27 +0200

    simplify logtail importer, no more signaling msgs

--- import_logtail.py
+++ import_logtail.py
@@ -29,30 +29,15 @@ def main():
     flush_thread = FlushThread(cube, flush_lock)
     flush_thread.start()
 
-    bytes_offset = 0
-
     for line in sys.stdin:
         event = json.loads(line)
-        if 'logtail_end' in event:
-            get_more = event['logtail_has_more']
-            bytes_offset = event['logtail_bytes_offset']
-            break
-
         event['data']['logtail'] = True
         with flush_lock:
             cube.add(event)
 
-    else:
-        raise ValueError("json stream ended unexpectedly")
-
     flush_thread.stop = True
     flush_thread.join()
     cube.flush()
-
-    print json.dumps({
-        'bytes_offset': bytes_offset,
-        'get_more': get_more,
-    })
 
 
 if __name__ == '__main__':
