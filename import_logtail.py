af8eca78404578415dff6f90135a58e8e8511489
Alex Morega <alex@grep.ro>
2011-11-27 21:50:01 +0200

    flush cube data every 10 seconds

--- import_logtail.py
+++ import_logtail.py
@@ -1,11 +1,33 @@
 import sys
 import logging
+import threading
+import time
 import simplejson as json
+
+
+FLUSH_INTERVAL = 10 # seconds
+
+
+def FlushThread(threading.Thread):
+    def __init__(self, cube, lock):
+        super(self, this).__init__()
+        self.cube = cube
+        self.lock = lock
+        self.stop = False
+
+    def run(self):
+        while not self.stop:
+            with self.lock:
+                self.cube.flush()
+            time.sleep(FLUSH_INTERVAL)
 
 
 def main():
     from to_cube import CubeUploader
     cube = CubeUploader("http://localhost:1080/1.0/event/put")
+    flush_lock = threading.Lock()
+    flush_thread = FlushThread(cube, flush_lock)
+    flush_thread.start()
 
     bytes_offset = 0
 
@@ -17,11 +39,14 @@ def main():
             break
 
         event['data']['logtail'] = True
-        cube.add(event)
+        with flush_lock:
+            cube.add(event)
 
     else:
         raise ValueError("json stream ended unexpectedly")
 
+    flush_thread.stop = True
+    flush_thread.join()
     cube.flush()
 
     print json.dumps({
