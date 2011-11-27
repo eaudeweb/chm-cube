336946cf22f95eb4c78f0064caf568b86462141a
Alex Morega <alex@grep.ro>
2011-11-27 21:04:45 +0200

    refactor cube event formatter to handle single line

--- apache_log.py
+++ apache_log.py
@@ -44,16 +44,20 @@ def parse_log(input_lines):
         yield entry
 
 
+def cube_event_for_entry(entry):
+    return {
+        'type': 'chm_eu',
+        'time': entry.datetime.isoformat(),
+        'data': {
+            'duration_ms': entry.duration * 10**3,
+            'path': entry.url,
+            'status': entry.status,
+            'size': entry.size,
+            'vhost': entry.vhost,
+        },
+    }
+
+
 def cube_events(log_file):
-    for n, entry in enumerate(parse_log(log_file)):
-        yield {
-            'type': 'chm_eu',
-            'time': entry.datetime.isoformat(),
-            'data': {
-                'duration_ms': entry.duration * 10**3,
-                'path': entry.url,
-                'status': entry.status,
-                'size': entry.size,
-                'vhost': entry.vhost,
-            },
-        }
+    for entry in parse_log(log_file):
+        yield cube_event_for_entry(entry)
